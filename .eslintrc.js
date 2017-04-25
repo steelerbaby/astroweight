f6d5664dc2d6f55a71009accd64c0dc07b443005
Rita McKerring <freejack619@gmail.com>
2017-04-25 16:58:12 -0700

    adding astroweight

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "globals": {
+    "module": true,
+    "require": true,
+    "describe": true,
+    "beforeEach": true,
+    "it": true
+  },
+  "rules": {
+    "no-global-assign": 0,
+    "no-console": 0
+  },
+  "extends": "eslint:recommended"
+};
